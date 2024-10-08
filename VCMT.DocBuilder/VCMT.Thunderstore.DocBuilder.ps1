1f89f9131c8ab9373a4413c325f443081baaa59a
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2024-10-08 14:51:18 -0700

    Fixed Versions

--- VCMT.DocBuilder/VCMT.Thunderstore.DocBuilder.ps1
+++ VCMT.DocBuilder/VCMT.Thunderstore.DocBuilder.ps1
@@ -5,7 +5,7 @@
 ### Written by Dragon of VoidCrewModdingTeam.
 ### Modified by: 
 ###
-### Script Version 1.1.4
+### Script Version 1.2.1
 ###
 ###
 ### This script was created for auto-generation/fill of release files for Void Crew mods.
